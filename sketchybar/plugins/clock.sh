3a9f03df166317db77c09834097bec87e33d906b
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2026-05-21 15:55:27 -0400

    style(sketchybar): bump clock icon to SF Pro Bold 11pt

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Date + time, updated every 10 seconds.
 
-sketchybar --set "$NAME" label="$(date '+%a %b %d  %H:%M')"
+sketchybar --set "$NAME" icon.font="SF Pro:Bold:11.0" label="$(date '+%a %b %d  %H:%M')"
